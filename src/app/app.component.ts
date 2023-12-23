876eb8ecc0ef57ab5ddd9baafd0ebe6ac8383f92
Kagami <shimura.kyohei7617@gmail.com>
2023-12-23 15:39:42 +0900

    auto: auto command push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+
+const COMMENTS: Comment[] = [
+  { name: 'Kagami', message: 'お疲れ！' },
+  { name: 'Kagami', message: '明日の夜空いてる？' },
+  { name: 'Alice', message: 'お疲れ！' },
+];
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
 }
