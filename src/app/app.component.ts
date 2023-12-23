a33a33c4c3040023d51ea89b451adef4146b48a3
Kagami <shimura.kyohei7617@gmail.com>
2023-12-23 15:58:20 +0900

    update: chat class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
 import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER: User = new User(1, 'Kagami');
+const ANOTHER_USER: User = new User(2, 'Alice');
 
 const COMMENTS: Comment[] = [
-  { name: 'Kagami', message: 'お疲れ！' },
-  { name: 'Kagami', message: '明日の夜空いてる？' },
-  { name: 'Alice', message: 'お疲れ！' },
+  new Comment(ANOTHER_USER, 'お疲れ！'),
+  new Comment(ANOTHER_USER, '明日の夜に時間もらえる？'),
+  new Comment(CURRENT_USER, 'お疲れさま！'),
+  new Comment(CURRENT_USER, '了解！ いつもの場所に19時で良い？'),
+  new Comment(ANOTHER_USER, '⭕'),
 ];
 
 @Component({
@@ -15,4 +21,5 @@ const COMMENTS: Comment[] = [
 })
 export class AppComponent {
   comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
